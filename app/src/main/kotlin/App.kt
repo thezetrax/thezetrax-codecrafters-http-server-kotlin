2c45550708128efc92fa5f1ae9bc8a8ce9d0f8a2
Zablon Dawit <zabjd22@gmail.com>
2025-11-24 13:17:13 +0300

    fix: flush before sending
    - Added ResponseBuilder

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -1,10 +1,41 @@
-import java.io.PrintWriter
+import java.io.BufferedWriter
+import java.io.ObjectOutputStream
+import java.io.OutputStreamWriter
 import java.net.ServerSocket;
 
 const val LINE_BREAK = "\r\n" // Carriage return + line feed (CRLF)
+const val PORT = 4221
+
+class ResponseBuilder {
+    private val headers = mutableMapOf<String, String>()
+    private var body: String = ""
+
+    fun setHeader(key: String, value: String): ResponseBuilder {
+        headers[key] = value
+        return this
+    }
+
+    fun setBody(body: String): ResponseBuilder {
+        this.body = body
+        return this
+    }
+
+    fun build(): String {
+        val length: Int = body.length
+        val stringBuilder = StringBuilder()
+        stringBuilder.append("HTTP/1.1 200 OK").append(LINE_BREAK)
+        headers["Content-Length"] = length.toString()
+        for ((key, value) in headers) {
+            stringBuilder.append("$key: $value").append(LINE_BREAK)
+        }
+        stringBuilder.append(LINE_BREAK) // Blank line between headers and body
+        stringBuilder.append(body)
+        return stringBuilder.toString()
+    }
+}
 
 fun main() {
-    val serverSocket = ServerSocket(4221)
+    val serverSocket = ServerSocket(PORT)
 
     //#region Socket Options
     // Since the tester restarts your program quite often, setting SO_REUSEADDR
@@ -12,13 +43,22 @@ fun main() {
     serverSocket.reuseAddress = true
     //#endregion
 
-    val clientSocket = serverSocket.accept() // Wait for connection from client.
-    println("accepted new connection")
-    val writer = PrintWriter(clientSocket.getOutputStream(), true)
+    println("listening on port $PORT")
 
-    val response = """
-        HTTP/1.1 200 OK$LINE_BREAK
-    """.trimIndent()
+    while (true) {
+        val clientSocket = serverSocket.accept() // Wait for connection from client.
 
-    writer.println(response)
+        // val ois = ObjectInputStream(clientSocket.getInputStream())
+        val out = BufferedWriter(
+            OutputStreamWriter(clientSocket.getOutputStream())
+        )
+
+        out.write(ResponseBuilder().build())
+        out.flush() // Ensure all data is sent out
+
+        println("accepted new connection")
+        clientSocket.close()
+    }
+
+    serverSocket.close()
 }
